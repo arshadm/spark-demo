6dc66a17ebf04433caf24f1724142105f1d3885e
Arshad Mahmood <arshadm@compuvision.co.uk>
2016-11-24 19:53:24 +0000

    updated

--- src/test/scala/io/spinor/sparkdemo/sql/SparkSqlSpec.scala
+++ src/test/scala/io/spinor/sparkdemo/sql/SparkSqlSpec.scala
@@ -1,8 +1,31 @@
 package io.spinor.sparkdemo.sql
 
-/**
-  * Created by arshadm on 22/11/2016.
-  */
-class SparkSqlSpec {
+import io.spinor.sparkdemo.util.DemoUtil
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.{SparkConf, SparkContext}
+import org.scalatest.{FlatSpec, Matchers}
+import org.slf4j.LoggerFactory
 
+/**
+  * Tests for processing various types of files via SparkSQL.
+  *
+  * @author A. Mahmood (arshadm@spinor.io)
+  */
+class SparkSqlSpec extends FlatSpec with DemoUtil with Matchers {
+  /** The logger. */
+  final val logger = LoggerFactory.getLogger(classOf[SparkSqlSpec])
+
+  "Loading JSON file via SparkSQL" should "be possible" in {
+    val sparkConf = new SparkConf()
+    sparkConf.setAppName("NaiveBayesDemo")
+    sparkConf.setMaster("local[2]")
+
+    val sparkContext = new SparkContext(sparkConf)
+    val sparkSession = SparkSession.builder().config(sparkConf).getOrCreate()
+    val sqlContext = sparkSession.sqlContext
+    import sqlContext.implicits._
+
+    val dframe = sqlContext.read.json("src/test/resources/sql/test1.json")
+    dframe.columns eq(Array("gloassary"))
+  }
 }
